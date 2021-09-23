640918ec226a0415093d9045bc241830f37238cb
emilio <emoxic.sama@gmail.com>
2021-09-23 08:52:12 -0500

    added text and fps

--- minimum_coed.py
+++ minimum_coed.py
@@ -12,6 +12,9 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils #Draws the points
 
+pTime = 0
+cTime = 0
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -27,5 +30,13 @@ while True:
             # mpDraw.draw_landmarks(img,handLms) #Original image + Single Hand
             mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS) #Original image + Single Hand + HandConnections
             
+    cTime = time.time() #current time
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)), (10,70),cv2.FONT_HERSHEY_DUPLEX,3,(255,0,255))
+
+
+    #display
     cv2.imshow("Image",img)
     cv2.waitKey(1)
