b484f70b4a6324f7320e4cce00e91e641c1574d4
emilio <emoxic.sama@gmail.com>
2021-09-26 21:35:34 -0500

    finished

--- hand_canvas/VirtualPainter.py
+++ hand_canvas/VirtualPainter.py
@@ -4,6 +4,13 @@ import time
 import os
 import handTrackingModule as htm
 
+
+#######
+brush_thickness = 15
+eraser_thickness = brush_thickness*4
+#######
+
+
 #########Open Files
 folderPath = "header"
 myList = os.listdir(folderPath)
@@ -17,14 +24,21 @@ print(len(overlay_list))#Check if read correctly
 header = overlay_list[0]
 #########Open Files
 
-drawColor = (255,255,255)
+#Initial draw color
+drawColor = (0,0,0)
 
 cap = cv2.VideoCapture(0)
 cap.set(3,1280) #Set feature id 3 (width) to value 1280
 cap.set(4,720) #Set height exactly to 720
 
 #For landmarks
-detector = htm.handDetector(min_detection_confidence = 0.90)
+detector = htm.handDetector(min_detection_confidence = 0.60)
+
+#For drawing
+xp,yp = 0,0
+
+
+imgCanvas = np.zeros((720,1280,3),np.uint8) #uint = 0-255 values
 
 
 while True:
@@ -36,6 +50,8 @@ while True:
     img = detector.findHands(img) 
     lmList = detector.findPosition(img,draw=False)
     if len(lmList) != 0:
+        
+
         #print(lmList)
         
         # (2.5 step) Index and middle
@@ -48,6 +64,8 @@ while True:
 
         # (4rd step) If selection node -> Two fingers are up 
         if fingers[1] and fingers[2]:
+            xp,yp = 0,0 #This fixes the bug where the line was drawn on switch
+
             #cv2.rectangle(img,(x1,y1-15),(x2,y2+15),(105, 66, 245),cv2.FILLED) #Draw square
             cv2.line(img,(x1,y1),(x2,y2),drawColor,3)
             print("Selection mode")            
@@ -67,20 +85,60 @@ while True:
                     drawColor = (10, 10, 252)
                 elif 1050 < x1 < 1200 : #It means it is clicking the first button
                     header = overlay_list[4] #The header is the fourth image
-                    drawColor = (0,0,0)
+                    drawColor = (0,0,0)            
 
         # (5th step) Drawing mode, index finger is up
-        if fingers[1] and fingers[2] == False:
-            cv2.circle(img,(x1,y1),10,drawColor,cv2.FILLED,5) #Draw dot BGR
+        if fingers[1] and fingers[2] == False:  
+            cv2.circle(img,(x1,y1),5,drawColor,cv2.FILLED,5) #Draw dot BGR
             print("Drawing Mode")
-        
+
+            if xp==0 and yp==0:
+                #first frame
+                xp,yp = x1,y1
+
+            #Drawing circles would have gaps, instead we'll draw lines
+            # cv2.line(img,(xp,yp),(x1,y1),drawColor,brush_thickness)
+
+            if drawColor == (0,0,0): #If eraser, then use big canvas
+                cv2.line(img,(xp,yp),(x1,y1),drawColor,eraser_thickness)
+                cv2.line(imgCanvas,(xp,yp),(x1,y1),drawColor,eraser_thickness)
+            else:
+                cv2.line(img,(xp,yp),(x1,y1),drawColor,brush_thickness)
+                cv2.line(imgCanvas,(xp,yp),(x1,y1),drawColor,brush_thickness)
+            xp,yp = x1,y1
+
+    #Converting the imgcanvas into a grey image
+    imgGray = cv2.cvtColor(imgCanvas,cv2.COLOR_BGR2GRAY) 
+    #Convert into a binary image and also inverting it
+    _, imgInv = cv2.threshold(imgGray,50,255,cv2.THRESH_BINARY_INV)
+    #The idea is
+    #   (1) Convert image into black and white
+    #   (2) Wherever there's black i want it to be white
+    #   (3) Wherever there's a color image i want it to be black
+    #   (4) Will create a mask and with white except the drawings as black
+    #   (5) In the original image, will make all of the previous image black 
+    #   (6) The merged options are black 
+    imgInv = cv2.cvtColor(imgInv,cv2.COLOR_GRAY2BGR)  #cONVERTING IT BACK (Cannot add grey image to color image)
+    img = cv2.bitwise_and(img,imgInv)
+    img = cv2.bitwise_or(img,imgCanvas)
+
+
+
+
+
 
     #(Normal Header) Overlay image -> Since it's a matrix we just need to define it's location
     img[0:125,0:1280] = header   #We just define the matrix content of img
+    
+    
+
+    ###ADD BOTH IMAGES TOGETHER
+    # img = cv2.addWeighted(img, 0.5,imgCanvas,0.5, 0)
     
 
 
 
     #Show video feed  
     cv2.imshow("Image",img)
+    # cv2.imshow("Canvas",imgCanvas)
     cv2.waitKey(1)
